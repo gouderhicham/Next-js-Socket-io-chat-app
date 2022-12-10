63473dcf71aa02628d8e7eac0952f8854920a731
gouder hicham <gouderhicham@gmail.com>
2022-12-11 00:31:34 +0100

    remove mongo db dependencie

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,17 +1,17 @@
-import clientPromise from "../../lib/mongo";
-export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db("hicham_db");
-  const allPosts = await db
-    .collection("allPosts")
-    .find({}, { projection: { _id: 0 } })
-    .toArray();
-  // db.collection("users").updateOne({_id : 123232039232} , { $push || $pull: {hobbies : "hello there " <= this is the elements that we are going to add to hoobbis array }})
+// import clientPromise from "../../lib/mongo";
+// export default async function handler(req, res) {
+//   const client = await clientPromise;
+//   const db = client.db("hicham_db");
+//   const allPosts = await db
+//     .collection("allPosts")
+//     .find({}, { projection: { _id: 0 } })
+//     .toArray();
+//   // db.collection("users").updateOne({_id : 123232039232} , { $push || $pull: {hobbies : "hello there " <= this is the elements that we are going to add to hoobbis array }})
 
-  // db.collection("allPosts").insertOne({
-  //   ip: "2323r23möl2m3rö2ml3r",
-  //   message: "goeirjgoeijrgergergergerg",
-  //   proImage: "georijgerger.com.jpg",
-  // });
-  res.json({ status: 200, data: allPosts });
-}
+//   // db.collection("allPosts").insertOne({
+//   //   ip: "2323r23möl2m3rö2ml3r",
+//   //   message: "goeirjgoeijrgergergergerg",
+//   //   proImage: "georijgerger.com.jpg",
+//   // });
+//   res.json({ status: 200, data: allPosts });
+// }
